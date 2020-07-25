f59a1833622c1f650646192659beebf72e98b47f
Katerina <panekkka@mail.ru>
2020-07-25 12:10:37 +0300

    initial commit

---
+++ Avia.java
@@ -0,0 +1,8 @@
+public class Avia {
+    public static void main(String[] args) {
+        float amount = 10000.50F;
+        float bonus = amount / 20;
+        System.out.println(bonus);
+
+    }
+}
