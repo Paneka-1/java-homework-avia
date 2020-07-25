1b54bac7c2cafc3c196b72efc6d957b222492767
Katerina <panekkka@mail.ru>
2020-07-25 16:05:19 +0300

    new in file

--- Avia.java
+++ Avia.java
@@ -1,6 +1,6 @@
 public class Avia {
     public static void main(String[] args) {
-        long amount = (long) 1000.60;
+        float amount = (float) 10000.60;
         int bonus = (int) (amount / 20);
         System.out.println(bonus);
 
